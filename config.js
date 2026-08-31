87c5c48f4d5197a88e7fbee34bb85ec7e32bb2ca
thibautpierre800-alt <thibautpierre800@gmail.com>
2026-08-31 22:51:35 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+/*
+ * Renseignez ces deux valeurs depuis Supabase > Project Settings > API.
+ * La clé "anon" est une clé publique prévue pour les applications web.
+ */
+window.BIA_SUPABASE = {
+  url: "https://gumgtbnlxvdwgdtuihjx.supabase.co",
+  anonKey: "sb_publishable_VadQs5VJc0asbmQWX0J2Ag_3nsm2k-s"
+};
